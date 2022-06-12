251397ba0003bebde4a8510bfdac02b3143ce0c3
Miguel Soriano <originalsorianom@gmail.com>
2022-06-12 06:54:45 -0700

    Updated index.html with a button. Also, add one more function to fetch an API  in script.js. When the button is clicked, information for a random drink should be displayed.

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,25 @@ function getCocktailList(ingredient) {
 }
 
 getRandomCocktail("gin")
+
+
+function drink1(element) {
+    return document.createElement(element);
+
+
+}
+
+
+//Fetching API for random testing.
+async function drinklist(){
+    var drink="https://www.thecocktaildb.com/api/json/v1/1/random.php";
+
+    let data =  await fetch(drink)
+      .then(response => response.json());
+      var {strDrink, strDrinkThumb, strInstructions} = data.drinks[0];
+      document.getElementById("Drinkname").textContent=strDrink;
+      document.getElementById("Drinkpictures").innerHTML = '<img src="' + strDrinkThumb + '"alt="Girl in a jacket" width="100" height="100" >';
+      document.getElementById("description").textContent='Instruction=' + strInstructions;
+
+      
+};
