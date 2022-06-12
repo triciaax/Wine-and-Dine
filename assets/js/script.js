b17e263c2adb5dc7f284d659790dce966c669275
Tricia <tricianghiem@gmail.com>
2022-06-12 12:13:32 -0500

    added second api function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,46 @@
-//master function to select random cocktail based on ingredient
-//function is async and await to get resolve the promises in console
+//main function for API calls
 async function getRandomCocktail(ingredient) {
-    var list = await getCocktailList(ingredient)
-    console.log(list)
+    //get list of cocktails from api
+    const list = await getCocktailList(ingredient);
+    //select random cocktail from list
+    const cocktail = selectRandomCocktail(list);
+    //get ingredients for selected cocktail
+    const ingredients = await getCocktailIngredients(cocktail);
+    console.log(cocktail)
+    console.log(ingredients)
 }
 
-//function to get a list of cocktails based on ingredient selected. 
-//The API url will be dynamic based on ingredient
 function getCocktailList(ingredient) {
-    var cocktailList = fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`)
+    var list = fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`)
         .then(response => response.json())
         .then(data => {
+            console.log(data)
             return data.drinks;
-        })
-    return cocktailList;
+        }
+        )
+    return list;
 }
 
-getRandomCocktail("gin")
+function selectRandomCocktail(list) {
+    // select random cocktail from list
+    //math.random() returns a random number between 0 and 1
+    //math.floor rounds down to the nearest integer
+    // multiply by list length to get a random index
+    let random = Math.floor(Math.random() * list.length);
+    console.log(random)
+    return list[random];
+}
+
+function getCocktailIngredients(cocktail) {
+    var ingredients = fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktail.idDrink}`)
+        .then(response => response.json())
+        .then(data => {
+            //this only shows the first ingredient
+            //you can get more in the same way. the ingredients and instructions are logged in the console.
+            console.log(data)
+            let ingredients = data.drinks[0].strIngredient1;
+            return ingredients;
+        }
+        )
+    return ingredients;
+}
