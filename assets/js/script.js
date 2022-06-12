34a373a6e3243ee7480e1bb66795534db4c9fdcc
Hannah Johnson <hgrace99@gmail.com>
2022-06-12 13:03:00 -0500

    testing food api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,28 @@
-<script src="www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin"></script>
+
+// chicken search
+var getData = fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(jsonObject) {
+    return console.log(jsonObject);
+  });
+
+// beef search
+var getData = fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=beef')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(jsonObject) {
+    return console.log(jsonObject);
+  });
+
+
+// tofu search
+var getData = fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=tofu')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(jsonObject) {
+    return console.log(jsonObject);
+  });
