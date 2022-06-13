9369033a56e8e26c7dd51d88c9f78dd73cbcd10a
Tricia <tricianghiem@gmail.com>
2022-06-13 15:15:18 -0500

    added rendering for ingredients

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 //global variables
 var searchCocktailTextEl = document.getElementById("search-cocktail");
 var submitCocktailButtonEl = document.getElementById("submit-cocktail");
+var ingredientEl = document.getElementById("ingredients");
 
 //main function for API calls
 async function getRandomCocktail(ingredient) {
@@ -52,42 +53,29 @@ function getCocktailIngredients(cocktail) {
   return recipe;
 }
 
-//render cocktail function
+//render cocktail recipe function
 function renderRecipe(recipe) {
-    let string = getRecipeFields(recipe);
-  headerInnerHtml = `
-<h5>${recipe.strDrink}</h5>
+    let measurementStrings = getRecipeFields(recipe);
+    var ingredientsHTML = `
+    <h5>${recipe.strDrink}</h5>
 
-<strong>INGREDIENTS</strong>
-<ul style="list-style-type:none;">
-  <li>${recipe.strMeasure1} ${recipe.strIngredient1}</li>
-  <li>${recipe.strMeasure2} ${recipe.strIngredient2}</li>
-  <li>${recipe.strMeasure3} ${recipe.strIngredient3}</li>
-  <li>${recipe.strMeasure4} ${recipe.strIngredient4}</li>
-  <li>${recipe.strMeasure5} ${recipe.strIngredient5}</li>
-  <li>${recipe.strMeasure6} ${recipe.strIngredient6}</li>
-  <li>${recipe.strMeasure7} ${recipe.strIngredient7}</li>
-  <li>${recipe.strMeasure8} ${recipe.strIngredient8}</li>
-  <li>${recipe.strMeasure9} ${recipe.strIngredient9}</li>
-  <li>${recipe.strMeasure10} ${recipe.strIngredient10}</li>
-  <li>${recipe.strMeasure11} ${recipe.strIngredient11}</li>
-  <li>${recipe.strMeasure12} ${recipe.strIngredient12}</li>
-  <li>${recipe.strMeasure13} ${recipe.strIngredient13}</li>
-  <li>${recipe.strMeasure14} ${recipe.strIngredient14}</li>
-  <li>${recipe.strMeasure15} ${recipe.strIngredient15}</li>
+    <strong>INGREDIENTS</strong>
+    <ul style="list-style-type:none;">
+    `
+    for (var i = 0; i < measurementStrings.length; i++) {
+        var innerHTML = `
+        <li>${measurementStrings[i]}</li>
+        `
+        ingredientsHTML += innerHTML;
+    }
+    ingredientsHTML += '</ul>';
+    ingredientEl.innerHTML =ingredientsHTML;
 
-</ul>
-
-<strong>INSTRUCTIONS</strong>
-<p>${recipe.strInstructions}</p>
-`;
 }
 
 // function to get fields from the response data
 function getRecipeFields(recipe) {
     var measurementStrings = [];
-    var ingredients = [];
-    var measures = [];
     for (var i = 1; i < 16; i++) {
         let ingredient = recipe[`strIngredient${i}`]
         if (ingredient) {
