4c0ea16fef7c0eb5e10d29545e4421435b93aaba
Hannah Johnson <hgrace99@gmail.com>
2022-06-14 23:17:38 -0500

    updating syntax

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,36 @@
 //global variables
-var searchCocktailTextEl = document.getElementById("search-cocktail");
-var submitCocktailButtonEl = document.getElementById("submit-cocktail");
+var searchMealTextEl = document.getElementById("search-meal");
+var submitMealButtonEl = document.getElementById("submit-meal");
 var ingredientEl = document.getElementById("ingredients");
 var instructionEl = document.getElementById("instructions");
 
 //main function for API calls
-async function getRandomCocktail(ingredient) {
-  //get list of cocktails from api
-  const list = await getCocktailList(ingredient);
-  //select random cocktail from list
-  const cocktail = selectRandomCocktail(list);
-  //get ingredients for selected cocktail
-  const recipe = await getCocktailIngredients(cocktail);
+async function getRandomMeal(ingredient) {
+  //get list of meals from api
+  const list = await getMealList(ingredient);
+  //select random meal from list
+  const meal = selectRandomMeal(list);
+  //get ingredients for selected meal
+  const recipe = await getMealIngredients(meals);
   renderRecipe(recipe);
-  console.log(cocktail);
+  console.log(meals);
   console.log(recipe);
 }
 
-function getCocktailList(ingredient) {
+function getMealList(ingredient) {
   var list = fetch(
-    `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`
+    `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`
   )
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      return data.drinks;
+      return data.meals;
     });
   return list;
 }
 
-function selectRandomCocktail(list) {
-  // select random cocktail from list
+function selectRandomMeal(list) {
+  // select random meal from list
   //math.random() returns a random number between 0 and 1
   //math.floor rounds down to the nearest integer
   // multiply by list length to get a random index
@@ -39,16 +39,16 @@ function selectRandomCocktail(list) {
   return list[random];
 }
 
-function getCocktailIngredients(cocktail) {
+function getMealIngredients(cocktail) {
   var recipe = fetch(
-    `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktail.idDrink}`
+    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${meals.idMeal}`
   )
     .then((response) => response.json())
     .then((data) => {
       //this only shows the first ingredient
       //you can get more in the same way. the ingredients and instructions are logged in the console.
       console.log(data);
-      let recipe = data.drinks[0];
+      let recipe = data.meals[0];
       return recipe;
     });
   return recipe;
@@ -56,54 +56,54 @@ function getCocktailIngredients(cocktail) {
 
 //render cocktail name and ingredients function
 function renderRecipe(recipe) {
-    let measurementStrings = getRecipeFields(recipe);
-    var ingredientsHTML = `
-    <h5>${recipe.strDrink}</h5>
+  let measurementStrings = getRecipeFields(recipe);
+  var ingredientsHTML = `
+    <h5>${recipe.strMeal}</h5>
+
     <strong>INGREDIENTS</strong>
     <ul style="list-style-type:none;">
-    `
-    for (var i = 0; i < measurementStrings.length; i++) {
-        var innerHTML = `
+    `;
+  for (var i = 0; i < measurementStrings.length; i++) {
+    var innerHTML = `
         <li>${measurementStrings[i]}</li>
-        `
-        ingredientsHTML += innerHTML;
-    }
-    ingredientsHTML += '</ul>';
-    ingredientEl.innerHTML =ingredientsHTML;
+        `;
+    ingredientsHTML += innerHTML;
+  }
+  ingredientsHTML += "</ul>";
+  ingredientEl.innerHTML = ingredientsHTML;
 
-
-    //render cocktail instructions and image function
-    var instructionsHTML= `
+  //render meal instructions and image functional
+  var instructionsHTML = `
     <strong>INSTRUCTIONS</strong>
     <p>${recipe.strInstructions}</p>
-    <img src="${recipe.strDrinkThumb}" style="width:250px;">`
+    <img src="${recipe.strMealThumb}" style="width:250px;">`;
 
-    instructionEl.innerHTML =instructionsHTML;
+  instructionEl.innerHTML = instructionsHTML;
 }
 
-// function to get fields from the response data
+//function to get fields from the response data
 function getRecipeFields(recipe) {
-    var measurementStrings = [];
-    for (var i = 1; i < 16; i++) {
-        let ingredient = recipe[`strIngredient${i}`]
-        if (ingredient) {
-            var measure = recipe[`strMeasure${i}`]
-            if (measure) {
-                var builtString = `${measure}${ingredient}`
-            } else {
-                var builtString = ingredient
-            }
-            measurementStrings.push(builtString);
-        }
+  var measurementStrings = [];
+  for (var i = 1; i < 16; i++) {
+    let ingredient = recipe[`strIngredient${i}`];
+    if (ingredient) {
+      var measure = recipe[`strMeasure${i}`];
+      if (measure) {
+        var builtString = `${measure}${ingredient}`;
+      } else {
+        var builtString = ingredient;
+      }
+      measurementStrings.push(builtString);
     }
-    console.log(measurementStrings)
-    return measurementStrings;
+  }
+  console.log(measurementStrings);
+  return measurementStrings;
 }
 
-//listener for cocktail button
-submitCocktailButtonEl.addEventListener("click", function () {
-  var cocktailIngredient = searchCocktailTextEl.value;
-  getRandomCocktail(cocktailIngredient);
+//listener for meal button
+submitMealButtonEl.addEventListener("click", function () {
+  var mealIngredient = searchMealTextEl.value;
+  getRandomMeal(mealIngredient);
 });
 
-getRandomCocktail("vodka");
+getRandomMeal("beef");
