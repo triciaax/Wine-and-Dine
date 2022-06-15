da7502fe59815433d07614682274bd18c9028e1d
Hannah Johnson <hgrace99@gmail.com>
2022-06-14 20:11:40 -0500

    restructuring with tricia and miguel's code because they are the best teammates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,109 @@
+//global variables
+var searchCocktailTextEl = document.getElementById("search-cocktail");
+var submitCocktailButtonEl = document.getElementById("submit-cocktail");
+var ingredientEl = document.getElementById("ingredients");
+var instructionEl = document.getElementById("instructions");
 
-// chicken search
-var getData = fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(jsonObject) {
-    return console.log(jsonObject);
-  });
+//main function for API calls
+async function getRandomCocktail(ingredient) {
+  //get list of cocktails from api
+  const list = await getCocktailList(ingredient);
+  //select random cocktail from list
+  const cocktail = selectRandomCocktail(list);
+  //get ingredients for selected cocktail
+  const recipe = await getCocktailIngredients(cocktail);
+  renderRecipe(recipe);
+  console.log(cocktail);
+  console.log(recipe);
+}
 
-// beef search
-var getData = fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=beef')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(jsonObject) {
-    return console.log(jsonObject);
-  });
+function getCocktailList(ingredient) {
+  var list = fetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      return data.drinks;
+    });
+  return list;
+}
+
+function selectRandomCocktail(list) {
+  // select random cocktail from list
+  //math.random() returns a random number between 0 and 1
+  //math.floor rounds down to the nearest integer
+  // multiply by list length to get a random index
+  let random = Math.floor(Math.random() * list.length);
+  console.log(random);
+  return list[random];
+}
+
+function getCocktailIngredients(cocktail) {
+  var recipe = fetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktail.idDrink}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      //this only shows the first ingredient
+      //you can get more in the same way. the ingredients and instructions are logged in the console.
+      console.log(data);
+      let recipe = data.drinks[0];
+      return recipe;
+    });
+  return recipe;
+}
+
+//render cocktail name and ingredients function
+function renderRecipe(recipe) {
+    let measurementStrings = getRecipeFields(recipe);
+    var ingredientsHTML = `
+    <h5>${recipe.strDrink}</h5>
+    <strong>INGREDIENTS</strong>
+    <ul style="list-style-type:none;">
+    `
+    for (var i = 0; i < measurementStrings.length; i++) {
+        var innerHTML = `
+        <li>${measurementStrings[i]}</li>
+        `
+        ingredientsHTML += innerHTML;
+    }
+    ingredientsHTML += '</ul>';
+    ingredientEl.innerHTML =ingredientsHTML;
 
 
-// tofu search
-var getData = fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=tofu')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(jsonObject) {
-    return console.log(jsonObject);
-  });
+    //render cocktail instructions and image function
+    var instructionsHTML= `
+    <strong>INSTRUCTIONS</strong>
+    <p>${recipe.strInstructions}</p>
+    <img src="${recipe.strDrinkThumb}" style="width:250px;">`
+
+    instructionEl.innerHTML =instructionsHTML;
+}
+
+// function to get fields from the response data
+function getRecipeFields(recipe) {
+    var measurementStrings = [];
+    for (var i = 1; i < 16; i++) {
+        let ingredient = recipe[`strIngredient${i}`]
+        if (ingredient) {
+            var measure = recipe[`strMeasure${i}`]
+            if (measure) {
+                var builtString = `${measure}${ingredient}`
+            } else {
+                var builtString = ingredient
+            }
+            measurementStrings.push(builtString);
+        }
+    }
+    console.log(measurementStrings)
+    return measurementStrings;
+}
+
+//listener for cocktail button
+submitCocktailButtonEl.addEventListener("click", function () {
+  var cocktailIngredient = searchCocktailTextEl.value;
+  getRandomCocktail(cocktailIngredient);
+});
+
+getRandomCocktail("vodka");
